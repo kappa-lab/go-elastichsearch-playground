405c58c57e9e55b3450a7424ceb2160a608012c5
gamiken <gamiken@kappa-lab.com>
2024-05-02 15:58:30 +0900

    create index, and doc

--- cmd/main_test.go
+++ cmd/main_test.go
@@ -1,11 +1,70 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
+	"io"
 	"testing"
 
+	elasticsearch8 "github.com/elastic/go-elasticsearch/v8"
 	"github.com/stretchr/testify/require"
 )
 
 func Test_main(t *testing.T) {
-	require.True(t, true)
+	client, err := elasticsearch8.NewDefaultClient()
+
+	require.NoError(t, err)
+	require.NotNil(t, client)
+
+	defer func() {
+		_, err := client.Indices.Delete([]string{"my_index"})
+		require.NoError(t, err)
+	}()
+
+	res, err := client.Indices.Create("my_index")
+	require.NoError(t, err)
+	require.NotNil(t, res)
+
+	res, err = client.Get("my_index", "1")
+	require.NoError(t, err)
+	require.NotNil(t, res)
+
+	b, err := io.ReadAll(res.Body)
+	require.NoError(t, err)
+
+	t.Log(string(b))
+	require.JSONEq(t, `{"_index":"my_index","_id":"1","found":false}`, string(b))
+
+	document := struct {
+		Name string `json:"name"`
+	}{
+		Name: "test_document",
+	}
+	data, err := json.Marshal(document)
+	require.NoError(t, err)
+
+	res, err = client.Create("my_index", "1", bytes.NewReader(data))
+	require.NoError(t, err)
+	require.NotNil(t, res)
+
+	res, err = client.Get("my_index", "1")
+	require.NoError(t, err)
+	require.NotNil(t, res)
+
+	b, err = io.ReadAll(res.Body)
+	require.NoError(t, err)
+
+	t.Log(string(b))
+	require.JSONEq(t, `{
+  "_index": "my_index",
+  "_id": "1",
+  "_version": 1,
+  "_seq_no": 0,
+  "_primary_term": 1,
+  "found": true,
+  "_source": {
+    "name": "test_document"
+  }
+}`, string(b))
+
 }
--- go.mod
+++ go.mod
@@ -2,10 +2,19 @@ module github.com/kappa-lab/go-elastichsearch-playground
 
 go 1.22.1
 
-require github.com/stretchr/testify v1.9.0
+require (
+	github.com/elastic/go-elasticsearch/v8 v8.13.1
+	github.com/stretchr/testify v1.9.0
+)
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/elastic/elastic-transport-go/v8 v8.5.0 // indirect
+	github.com/go-logr/logr v1.3.0 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	go.opentelemetry.io/otel v1.21.0 // indirect
+	go.opentelemetry.io/otel/metric v1.21.0 // indirect
+	go.opentelemetry.io/otel/trace v1.21.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
